a5a82a112f26b241bcc0e77ce868ef10419c6524
BuildTools <unconfigured@null.spigotmc.org>
2025-04-05 14:21:03 +0200

    established initial server files + frontend connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react'
 import appLogo from './assets/App Logo.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    console.log(response.data.bruhs);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
   return (
     <>
