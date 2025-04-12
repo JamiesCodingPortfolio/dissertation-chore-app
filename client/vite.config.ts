b267299e3ff979b47b7e00c75a4831626293c6a3
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-04-12 18:48:11 +0200

    changes to vite config to enable SSL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,17 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import fs from "fs"
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
+    https:{
+      key: fs.readFileSync(`../private.key`),
+      cert: fs.readFileSync(`../certificate.crt`),
+    },
+    port: 443
   },
   plugins: [react(), tailwindcss()],
   resolve: {
