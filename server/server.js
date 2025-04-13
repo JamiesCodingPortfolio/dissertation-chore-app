f5f83dac0875b32d281969263443fa1e3fa919dd
BuildTools <unconfigured@null.spigotmc.org>
2025-04-13 14:59:19 +0200

    changed server js origin port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:443",
 };
 
 app.use(cors(corsOptions));
