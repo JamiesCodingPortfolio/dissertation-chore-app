0368a81096661873d69bbda48ccd925a51a8c5f4
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-04-15 03:16:23 +0100

    updates to serverJS to use ES modules instead of CommonJS

--- server/server.js
+++ server/server.js
@@ -1,22 +1,28 @@
-require("dotenv").config({ path: "../.env" });
-var fs  = require("fs");
-var http = require("http");
-var https = require("https");
+import dotenv from 'dotenv';
+import fs from 'fs';
+import https from 'https';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: join(__dirname, '../.env') });
 
-const express = require("express");
+const privateKey = fs.readFileSync(join(__dirname, '../private.key'), 'utf-8');
+const certificate = fs.readFileSync(join(__dirname, '../certificate.key'), 'utf-8');
+
 const app = express();
-const cors = require("cors");
-const corsOptions = {
-    origin: "0.0.0.0",
-};
-
-app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
-    res.json({bruhs:["bruh", "bruhs"]});
+  res.json({ bruhs: ["bruh", "bruhs"] });
 });
 
-app.listen(443, () => {
-    console.log("Server started active on port 443");
+const httpsServer = https.createServer(
+  { key: privateKey, cert: certificate },
+  app
+);
+
+httpsServer.listen(443, () => {
+  console.log("HTTPS Server started on port 443");
 });
