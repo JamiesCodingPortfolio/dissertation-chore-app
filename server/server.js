c70be3d49a79de5844a90ea9911d6caff4f909d1
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-04-15 04:50:03 +0200

    updated port number

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 dotenv.config({ path: join(__dirname, '../.env') });
 
 const privateKey = fs.readFileSync(join(__dirname, '../private.key'), 'utf-8');
-const certificate = fs.readFileSync(join(__dirname, '../certificate.key'), 'utf-8');
+const certificate = fs.readFileSync(join(__dirname, '../certificate.crt'), 'utf-8');
 
 const app = express();
 
@@ -23,6 +23,6 @@ const httpsServer = https.createServer(
   app
 );
 
-httpsServer.listen(443, () => {
-  console.log("HTTPS Server started on port 443");
+httpsServer.listen(8443, () => {
+  console.log("HTTPS Server started on port 8443");
 });
