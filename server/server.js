49c7ceb5c6994ee8a9a20c7f4607442b9aefe9c7
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-05-05 18:11:14 +0100

    changes to server.js to use environment variables and allow localhost development

--- server/server.js
+++ server/server.js
@@ -7,10 +7,9 @@ import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 dotenv.config({ path: join(__dirname, '../.env') });
 
-const privateKey = fs.readFileSync(join(__dirname, '../private.key'), 'utf-8');
-const certificate = fs.readFileSync(join(__dirname, '../certificate.crt'), 'utf-8');
 
 const app = express();
 
@@ -20,11 +19,27 @@ app.get("/api", (req, res) => {
   res.json({ bruhs: ["bruh", "bruhs"] });
 });
 
-const httpsServer = https.createServer(
-  { key: privateKey, cert: certificate },
-  app
-);
+const HTTPS_ENABLED = process.env.HTTPS_ENABLED === 'true'
+const HTTP_PORT = process.env.HTTP_PORT_NUMBER
+const HTTPS_PORT = process.env.HTTPS_PORT_NUMBER
 
-httpsServer.listen(2053, '0.0.0.0', () => {
-  console.log("HTTPS Server started on port 2053");
-});
+if (HTTPS_ENABLED){
+
+  const privateKey = fs.readFileSync(join(__dirname, '../private.key'), 'utf-8');
+  const certificate = fs.readFileSync(join(__dirname, '../certificate.crt'), 'utf-8');
+
+  const httpsServer = https.createServer(
+    { key: privateKey, cert: certificate },
+    app
+  )
+
+  httpsServer.listen(HTTPS_PORT, '0.0.0.0', () => {
+    console.log("HTTPS Server started on port 2053")
+  })
+}
+
+else{
+  app.listen(HTTP_PORT, '127.0.0.1', () => {
+    console.log('Server running on port 8080');
+  })
+}
