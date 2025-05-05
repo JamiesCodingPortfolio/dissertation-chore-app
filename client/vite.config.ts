2d0207dce819f1311292c7a3bece2a04ef27f04e
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-05-05 05:16:20 +0100

    changes to vite config to allow testing on local system

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,16 +3,26 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import fs from "fs"
+import dotenv from "dotenv"
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") })
+
+const HTTPS_ENABLED = process.env.HTTPS_ENABLED === "true"
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    https:{
-      key: fs.readFileSync(`../private.key`),
-      cert: fs.readFileSync(`../certificate.crt`),
-    },
-    port: 443
+    ...(HTTPS_ENABLED && {
+      https: {
+        key: fs.readFileSync(`../private.key`),
+        cert: fs.readFileSync(`../certificate.crt`),
+      },
+      port: 443,
+    }),
+    ...(!HTTPS_ENABLED && {
+      port: 3000,
+    })
   },
   plugins: [react(), tailwindcss()],
   resolve: {
